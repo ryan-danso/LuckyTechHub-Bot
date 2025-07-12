280dd49a1fd3e18f76b6c25840c05f2ae81a621f
ryan-danso <ryandanso125@gmail.com>
2025-07-12 12:14:07 +0000

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
   packname: 'Lucky Tech Hub Bot',
   author: '‎',
-  botName: "Lucky Tech Hub Bot",
-  botOwner: 'Lucky218', // Your name
-  ownerNumber: '256789101112', //Set your number here without + symbol, just add country code & number without any space
+  botName: "Ryan Danso Bot",
+  botOwner: 'Ryan Danso', // Your name
+  ownerNumber: '233543113918', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
